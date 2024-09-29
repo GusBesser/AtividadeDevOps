b22301d01d15791890f7cb6b344e3b9e61f8ddf7
GusBesser <esser.gustavo@gmail.com>
2024-09-29 16:51:18 -0300

    unit test workflow
    criação de workflow de unit testing em CI.yml

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 from src.main import *
 
 def test_somar():
-    assert somar(10 ,10) == 20
+    assert somar(10, 10) == 20
 
 def test_subtrair():
     assert subtrair(10, 10) == 0
 
 def test_multiplicar():
-    assert multiplicar(10 ,10) == 100
+    assert multiplicar(10, 10) == 100
 
 def test_dividir():
-    assert dividir(10 ,5) == 2.0
+    assert dividir(10, 5) == 2.0
 
 def test_resto():
-    assert resto(9 ,2) == 1
+    assert resto(9, 2) == 1
 
 print(somar(10, 10))
 print(subtrair(10, 10))
