f34b12180b7bc3c610f4e08e5ab043eca9c58bf6
GusBesser <esser.gustavo@gmail.com>
2024-09-29 16:38:31 -0300

    test file creation

---
+++ test.py
@@ -0,0 +1,28 @@
+from src.main import *
+
+def test_somar():
+    assert somar(10 ,10) == 20
+
+def test_subtrair():
+    assert subtrair(10, 10) == 0
+
+def test_multiplicar():
+    assert multiplicar(10 ,10) == 100
+
+def test_dividir():
+    assert dividir(10 ,5) == 2.0
+
+def test_resto():
+    assert resto(9 ,2) == 1
+
+print(somar(10, 10))
+print(subtrair(10, 10))
+print(multiplicar(10, 10))
+print(dividir(10, 5))
+print(resto(9, 2))
+
+test_somar()
+test_subtrair()
+test_multiplicar()
+test_dividir()
+test_resto()
